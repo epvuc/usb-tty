b36c3d2da6fd5e1bc4284f507a85234904b8e795
eric <eric@limpoc.com>
2016-02-16 21:27:12 -0800

    clean up

--- baudot.h
+++ baudot.h
@@ -1,5 +1,8 @@
 char baudot_to_ascii(char);
 char ascii_to_baudot(char);
+int tty_putchar(char, FILE *);
+int tty_putchar_raw(char);
+
 
 extern volatile int8_t flag_tx_rdy;
 extern volatile int8_t flag_tx_busy;
---
+++ usb_serial_getstr.h
@@ -0,0 +1,3 @@
+char usb_serial_getchar(void);
+void usb_serial_putchar(char);
+int usb_serial_getstr(char *, int);
