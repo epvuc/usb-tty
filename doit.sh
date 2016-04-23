d380f94cefe2fde7c4c1f2d4045506a029240f8e
eric <eric@limpoc.com>
2016-04-22 23:22:41 -0700

    use dfu bootloader for mega32u2

--- doit.sh
+++ doit.sh
@@ -1,3 +1,3 @@
 avrdude -p atmega16u2 -P /dev/ttyACM0 -c stk500v2 -e -U hfuse:w:0xd0:m	
 avrdude -p atmega16u2 -P /dev/ttyACM0 -c stk500v2 -e -U lfuse:w:0xde:m	
-avrdude -p atmega16u2 -P /dev/ttyACM0 -c stk500v2 -e -U flash:w:BootloaderDFU-16u2.hex
+avrdude -p atmega16u2 -P /dev/ttyACM0 -c stk500v2 -e -U flash:w:BootloaderDFU-32u2.hex
