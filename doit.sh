adde10db0b3f8a27fa0ed0629bb9385cadcd067e
eric <eric@limpoc.com>
2016-04-25 23:00:06 -0700

    add some docs for how to install on windows 8 w/ driver signing.

--- doit.sh
+++ doit.sh
@@ -1,3 +1,6 @@
+#avrdude -p atmega32u2 -P /dev/ttyACM0 -c stk500v2 -U hfuse:w:0xd0:m
+#avrdude -p atmega32u2 -P /dev/ttyACM0 -c stk500v2 -U lfuse:w:0xde:m
+
 avrdude -p atmega32u2 -P /dev/ttyACM0 -c stk500v2 -e -U hfuse:w:0xd0:m	
 avrdude -p atmega32u2 -P /dev/ttyACM0 -c stk500v2 -e -U lfuse:w:0xde:m	
 avrdude -p atmega32u2 -P /dev/ttyACM0 -c stk500v2 -e -U flash:w:BootloaderDFU-32u2.hex
